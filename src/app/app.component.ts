946d51e31bdc7bb623c170e6c2715d568a8921bb
AbhinayB <abhinay.bali@gmail.com>
2019-02-01 14:26:03 -0500

    Assignment Fixed the lint errors, made comments and removed the logs.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent implements OnInit{
-  arraystring:String;
-  arrayItems:Number[]; 
-  currentItem:Number;
-  positionShift:Number;
-  isValid:Boolean;
-  isDuplicate:Boolean;
-  notFound:Boolean;
-  resultPosition:Number;
-  currentPosition:Number;
-  
+export class AppComponent implements OnInit {
+  arraystring: string;
+  arrayItems: number[];
+  currentItem: number;
+  positionShift: number;
+  isValid: boolean;
+  isDuplicate: boolean;
+  notFound: boolean;
+  resultPosition: number;
+  currentPosition: number;
 
-  ngOnInit(){
+
+  ngOnInit() {
     this.arrayItems = [];
     this.currentItem = 0;
     this.positionShift = 0;
@@ -29,54 +29,54 @@ export class AppComponent implements OnInit{
     this.currentPosition = 0;
   }
 
-  processData(){
+  processData() {
     this.arrayItems = [];
     this.resultPosition = 0;
     this.currentPosition = 0;
     this.isValid = false;
     this.isDuplicate = false;
     this.notFound = false;
-    var regexpr = new RegExp("[0-9]+(,[0-9]+)*");
+    const regexpr = new RegExp('[0-9]+(,[0-9]+)*');
 
-    if(!this.arraystring.match(regexpr)){
+    if (!this.arraystring.match(regexpr)) {
       this.isValid = true;
       return;
     }
 
-    var arr = this.arraystring.split(",");
+    const arr = this.arraystring.split(',');
 
-    this.arraystring.split(",").map((ele)=>{
-      this.arrayItems.push(parseInt(ele));
+    this.arraystring.split(',').map((ele) => {
+      this.arrayItems.push(parseInt(ele, 10));
     });
-    if(this.hasDuplicates(this.arrayItems)){
+    if (this.hasDuplicates(this.arrayItems)) {
       this.isDuplicate = true;
       return;
     }
-    this.arrayItems.sort(function(a:number,b:number){return a - b});
-    this.cycle(this.currentItem,this.positionShift,this.arrayItems[0],this.arrayItems[this.arrayItems.length-1]);
+    this.arrayItems.sort((a: number, b: number) => (a - b));
+    this.cycle(this.currentItem, this.positionShift, this.arrayItems[0], this.arrayItems[this.arrayItems.length - 1]);
   }
+   // cycle function will process your results and give you the current item.
+  cycle(current, imove, start, end) {
 
-  cycle(current,imove,start,end){
-    
-    let index = this.arrayItems.findIndex((ele)=>{
+    let index = this.arrayItems.findIndex((ele) => {
         return ele == current;
     });
-    if(index == -1){
+    if (index === -1) {
       this.notFound = true;
       return;
     }
     this.currentPosition = index;
-    index = index+parseInt(imove);
-    if(index<0){
-      index = (index%(this.arrayItems.length))+(this.arrayItems.length);
+    index = index + parseInt(imove, 10);
+    if (index < 0) {
+     // is the logic for negative circular dependency if exceeds length of array
+      index = (index % (this.arrayItems.length)) + (this.arrayItems.length);
     }
-    index = index%(this.arrayItems.length);
+     // is the logic for possitive circular dependency if exceeds length of array
+    index = index % (this.arrayItems.length);
     this.resultPosition = index;
-    console.log(this.currentPosition);
-    console.log(this.resultPosition);
-    
-}
 
+}
+   // has duplicates checks if the array has duplicates or not
   hasDuplicates(array) {
     return (new Set(array)).size !== array.length;
   }
